bfd50f9652bffc38be8a02fd46c18a69b37e7f95
rodrigoreisrq <rodrigoreis15822@email.com>
2026-06-10 16:29:53 -0300

    feat: versão inicial do projeto

---
+++ ControleRemoto.java
@@ -0,0 +1,131 @@
+package encapsulamentoguanabara;
+
+public class ControleRemoto implements Controlador {
+    //Atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+
+    //Metodos especiais
+
+    // Getters e Setters private -- não permitem que possa acessá-los na classe principal
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean getLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean getTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+
+
+    }
+
+    //Metodos abstratos
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        if (this.getLigado()) {
+            System.out.println("--------MENU---------");
+            System.out.println("Esta ligado? " + this.getLigado());
+            System.out.println("Esta tocando? " + this.getTocando());
+            System.out.println("Volume: " + this.getVolume());
+            for (int i = 0; i <= this.getVolume(); i++) {
+                System.out.print("|");
+            }
+        }else{
+            System.out.println("Controle está desligado");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu.");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() + 1);
+        } //else {
+
+        //}
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() - 1);
+        } //else {
+
+       // }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+            System.out.println("Muted...");
+
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+            System.out.println("Tocando");
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.getLigado() && !this.getTocando()) {
+            this.setTocando(true);
+            System.out.println("Playing...");
+        } //else {
+            //System.out.println("Impossivel play");
+        //}
+    }
+
+    @Override
+    public void pause() {
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
+            System.out.println("Paused...");
+        } //else {
+            //System.out.println("Impossivel pausar");
+        //}
+    }
+
+}
---
+++ ControleRemotoMain.java
@@ -0,0 +1,14 @@
+package encapsulamentoguanabara;
+
+public class ControleRemotoMain {
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+        controle.ligar();
+        controle.play();
+        controle.pause();
+        controle.maisVolume();
+        controle.maisVolume();
+        controle.ligarMudo();
+        controle.abrirMenu();
+    }
+}
